01086260e008ed440f5182b1f3c24adae30ee6d9
BuildTools <unconfigured@null.spigotmc.org>
2023-03-19 20:26:04 +0900

    Fixed entity explode event works sans-y

--- src/main/java/com/jho5245/cucumbery/util/blockplacedata/ExplodeEventManager.java
+++ src/main/java/com/jho5245/cucumbery/util/blockplacedata/ExplodeEventManager.java
@@ -5,6 +5,7 @@ import com.jho5245.cucumbery.util.storage.no_groups.ItemStackUtil;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import de.tr7zw.changeme.nbtapi.NBTType;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -21,6 +22,11 @@ public class ExplodeEventManager
       ItemStack itemStack = BlockPlaceDataConfig.getItem(location);
       if (ItemStackUtil.itemExists(itemStack))
       {
+        // TNT로 폭발한 블록이 다른 커스텀 TNT일 경우 드롭하지 않고 폭발
+        if (itemStack.getType() == Material.TNT)
+        {
+          continue;
+        }
         removeList.add(block);
         NBTItem nbtItem = new NBTItem(itemStack);
         int blockTier = nbtItem.hasTag(MiningManager.BLOCK_TIER) && nbtItem.getType(MiningManager.BLOCK_TIER) == NBTType.NBTTagInt ? nbtItem.getInteger(MiningManager.BLOCK_TIER) : MiningManager.getVanillaBlockTier(block.getType());
